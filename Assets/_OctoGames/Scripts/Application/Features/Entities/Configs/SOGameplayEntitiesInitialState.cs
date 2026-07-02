efd6e69d134863615be342a0a4d6e0fea8de875e
Viktor Gubkov <gubkoff@gmail.com>
2026-07-03 00:46:39 +0500

    Remove default values

--- Assets/_OctoGames/Scripts/Application/Features/Entities/Configs/SOGameplayEntitiesInitialState.cs
+++ Assets/_OctoGames/Scripts/Application/Features/Entities/Configs/SOGameplayEntitiesInitialState.cs
@@ -8,12 +8,7 @@ namespace OctoGames.App.Features.Entities
         menuName = "OctoGames/Gameplay/Entities Initial State")]
     public sealed class SOGameplayEntitiesInitialState : ScriptableObject
     {
-        [SerializeField] private GameplayEntityData[] _entities =
-        {
-            new(GameplayEntityType.Enemy, GameplayEntityState.Active, new Vector3(-2f, 0.5f, 0f)),
-            new(GameplayEntityType.Interactable, GameplayEntityState.Active, new Vector3(0f, 0.5f, 2f)),
-            new(GameplayEntityType.StoryActor, GameplayEntityState.Disabled, new Vector3(2f, 0.5f, -1f))
-        };
+        [SerializeField] private GameplayEntityData[] _entities;
 
         public IReadOnlyList<GameplayEntityData> Entities => _entities;
     }
